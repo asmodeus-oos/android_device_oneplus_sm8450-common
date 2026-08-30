96cd93258d23539b29afe726c6b87547a8653a95
asmodeus-oos <mohamed20020093@gmail.com>
2026-08-30 16:42:40 +0000

    sm8450-common: add libaudiobase needed to libwfdservice

--- extract-files.py
+++ extract-files.py
@@ -68,6 +68,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .add_needed('libaudiobase.so')
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     ('system_ext/bin/horae', 'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so'): blob_fixup()
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so')
